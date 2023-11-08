2063698a5ebf9e4d6e8a21e6d8ae94bca9e18433
Tasin007 <mdtasinalam.mta@gmail.com>
2023-11-08 23:17:27 +0600

    Job tabs issue and all jobs hero issue solving

--- src/components/Shared/JobTabs.jsx
+++ src/components/Shared/JobTabs.jsx
@@ -19,3 +19,41 @@ const JobTabs = () => {
     e.preventDefault();
     setActiveTab(name);
   };
+
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000/api/v1/jobs'); // Replace with your actual API endpoint
+        setJobs(response.data);
+      } catch (error) {
+        console.error('Error fetching jobs:', error);
+      }
+    };
+    fetchJobs();
+  }, []);
+
+  const onsiteJobs = jobs.filter(job => job.category === 'onsite');
+  
+  const remoteJobs = jobs.filter(job => job.category === 'remote');
+  
+  const hybridJobs = jobs.filter(job => job.category === 'hybrid');
+
+  const parttimeJobs = jobs.filter(job => job.category === 'parttime');
+
+
+  return (
+    <div className='mt-5'>
+      <div className="flex flex-wrap justify-center -mb-px">
+        {tabs.map((tab) => (
+          <button
+            key={tab.name}
+            role="tab"
+            aria-selected={activeTab === tab.name}
+            className={`tab tab-bordered md:tab-lg ${activeTab === tab.name ? 'tab-active' : ''}`}
+            onClick={(e) => handleClick(e, tab.name)}
+          >
+            {tab.label}
+          </button>
+        ))}
+      </div>
